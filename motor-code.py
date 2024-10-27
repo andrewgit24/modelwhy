bb8233f3345ae7a2d12a68950bf7a4cdcbd67626
andrewgit24 <160302234+andrewgit24@users.noreply.github.com>
2024-10-27 10:34:44 -0500

    Update motor-code.py
    New way to do move_straight

--- motor-code.py
+++ motor-code.py
@@ -1,8 +1,12 @@
 from hub import port, motion_sensor
 import runloop, motor_pair, motor
 
+# async function to move motor pair straight
+async def move_motor_pair(steering, speed):
+    motor_pair.move(motor_pair.PAIR_1, steering, velocity=speed)
+
 # motor move straight forward and backward
-async def move_straight(distance, speed, debug = False):
+async def move_straight(distance, speed, KP=0.3, debug = False):
     #debugging
     if debug:
         print('===== move_straight =====')
@@ -11,8 +15,8 @@ async def move_straight(distance, speed, debug = False):
 
     #initialize parameters and motor
     angle = 0
-    KP=0.3
     KI=0.0
+    timer = 200
     motion_sensor.reset_yaw(0)
     motor.reset_relative_position(port.D, 0)
     degree_factor=360/7.675
@@ -30,7 +34,8 @@ async def move_straight(distance, speed, debug = False):
         current_error= angle - yaw
         past_error = past_error +current_error
         correction = int(current_error *KP + past_error*KI)
-        motor_pair.move(motor_pair.PAIR_1, correction, velocity=speed)
+        runloop.run(move_motor_pair(correction, speed))
+        runloop.sleep_ms(timer)
     motor_pair.stop(motor_pair.PAIR_1)
 
     # debugging
@@ -39,7 +44,7 @@ async def move_straight(distance, speed, debug = False):
         print('Robot off course by ' + str(yaw) + " degrees")
 
 # turn motor at an angle
-async def turn_for_degrees(degrees, speed, debug = False):
+async def turn_for_degrees(degrees, speed, KP=0.3, debug = False):
     #debugging
     if debug:
         print('===== turn_for_degrees =====')
@@ -47,8 +52,8 @@ async def turn_for_degrees(degrees, speed, debug = False):
         print('speed = ' + str(speed))
 
     # initialize parameters and motor
-    KP=0.3
-    KI=0.005
+    KI=0.0
+    timer=200
     motion_sensor.reset_yaw(0)
     steer = 100
     adj_factor=180/112.5
@@ -67,9 +72,10 @@ async def turn_for_degrees(degrees, speed, debug = False):
 
     # current errors
     error = degrees - abs(yaw)
+    past_error = 0
     counter = 0
     while abs(error) > 2 and counter < 10:
-        correction = int(error)
+        correction = int(error * KP + past_error * KI)
         if correction > 5 and counter > 3:
             correction = 5
         elif correction < -5 and counter > 3:
@@ -82,6 +88,7 @@ async def turn_for_degrees(degrees, speed, debug = False):
             print('correction = ' + str(correction))
             print("current angle = " + str(yaw))
         error = degrees - abs(yaw)
+        past_error = past_error + error
 
     # get actual degrees
     if debug:
@@ -109,8 +116,8 @@ async def main():
     motion_sensor.reset_yaw(0)
     #await runloop.until(motion_sensor.stable)
 
-    #await move_straight(2, 250, True)
-    await turn_for_degrees(90, 150)
+    #await move_straight(100, 300, KP=0.75)
+    await turn_for_degrees(-30, 250, KP=1.1, debug=True)
     #await move_straight(23, 400)
     #await turn_for_degrees(90, 200)
     #await move_straight(1, 400)
